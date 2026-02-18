2a9971be410b81ed848fb41ae93b9343b4daecc3
gustavo henrique <gustavovieirapereira99@gmail.com>
2026-02-18 12:44:02 -0300

    primeira aula de programação

---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 4000 # variavel
+numero_quebrado = 5.75 #variavel float
+palmeiras_tem_mundial = True # variavel booleana 
+texto = "corinthians maior time do brasil" # variavel de texto 
+
+
+print(palmeiras_tem_mundial)
+print(numero)
+print(numero_quebrado)
+print(texto)
