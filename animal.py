1a0d5bef16bfa977af2352c5fd9396bf5ceddbf3
gustavo <gustavovieirapereira99@gmail.com>
2026-02-19 10:08:44 -0300

    animal

---
+++ animal.py
@@ -0,0 +1,10 @@
+nome = "leao" # variavel de texto
+idade = 3 # variavel de int
+peso= 30.06 # variavel de float
+sexo = "macho" # variavel de texto
+
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo)
