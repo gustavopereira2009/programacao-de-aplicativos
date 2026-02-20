c83f973456a33bc42323c9baf08713ae961fa6a0
gustavo <gustavovieirapereira99@gmail.com>
2026-02-20 09:08:30 -0300

    matematica

---
+++ matematica.py
@@ -0,0 +1,11 @@
+valor1 = int(input("digite o primeiro numero"))
+valor2 = int(input("digite o primeiro numero"))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+mutiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+print("a soma e:" , soma)
+print("subtracao e:" , subtracao)
+print("multiplicacao e :" , mutiplicacao)
