02693df2705e36332cf4ba037ee3ce2fd2b3e541
gustavo <gustavovieirapereira99@gmail.com>
2026-03-12 10:04:33 -0300

    12/03/26

---
+++ ex06.py
@@ -0,0 +1,6 @@
+
+filmes = ["Interestelar", "Matrix", "Clube da Luta", "A Origem", "O Poderoso Chefão"]
+
+# Imprimindo o primeiro e o último filme da lista
+print(filmes[0])   # primeiro filme
+print(filmes[-1])  # último filme
