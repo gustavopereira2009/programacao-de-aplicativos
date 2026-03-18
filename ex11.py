a76b892c184053d0106b661a723481155e2ea914
gustavo <gustavovieirapereira99@gmail.com>
2026-03-18 12:40:49 -0300

    18/03/26

---
+++ ex11.py
@@ -0,0 +1,14 @@
+# Lista inicial
+fila = ["Ana", "Bruno", "Caio"]
+
+# Daniel entra no final da fila
+fila.append("Daniel")
+
+# Zuleica entra no início da fila
+fila.insert(0, "Zuleica")
+
+# Imprimindo a lista final
+print("Fila final:", fila)
+
+# Quantidade de pessoas na fila
+print("Total de pessoas:", len(fila))
