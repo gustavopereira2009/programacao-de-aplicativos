50cbf181a8e5e00e71625fdad245865fb267c23b
gusavo <gustavovieirapereira99@gmail.com>
2026-07-29 12:31:51 -0300

    29/07/26

--- bug.exercicios/ex1.py
+++ bug.exercicios/ex1.py
@@ -5,12 +5,23 @@ def inicializar_banco():
     cursor = conexao.cursor()
 
     cursor.execute('''
-                   CREATE TABLE IF NOT EXISTS escolas(
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   nome TEXT NOT NULL
-                   )
-                   ''')
-   
-    # Faltou o conexao.commit() para salvar as alteracoes
+        CREATE TABLE IF NOT EXISTS escolas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL
+        )
+    ''')
+
+    nome = input("Digite o nome da escola: ")
+
+    cursor.execute('''
+        INSERT INTO escolas(nome)
+        VALUES(?)
+    ''', (nome,))
+
     conexao.commit()
     conexao.close()
+
+inicializar_banco()
+
+
+# Faltou o conexao.commit() para salvar as alteracoes
